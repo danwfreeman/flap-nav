0da6b65ddedd3809f6ca010feeb7fc6c96a8d975
Dan Freeman <freebird@dans-mbp.mynetworksettings.com>
2023-07-17 01:26:50 -0400

    config

--- docs/publish.md
+++ docs/publish.md
@@ -1,5 +1,5 @@
 ### How to pubish to npm
 
 - update version number in `package.json`
-- if first time on a new machine run `npm adduser`
+- if first time on a new machine run `npm adduser` (will generate a new access token in the npmjs account)
 - run `npm publish`
--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
 import postcss from 'rollup-plugin-postcss'
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 
 // the entry point for the library
@@ -22,43 +24,35 @@ var config = []
 
 
 MODE.map((m) => {
-    var conf = {
-        input: input,
-        output: {
-            // then name of your package
-            name: "FlapNav",
-            file: `dist/index.${m.fomart}.js`,
-            format: m.fomart,
-            exports: "auto",
-            globals: {
-              react: 'React'
-            }
-        },
-        // this externelizes react to prevent rollup from compiling it
-        external: ["react", /@babel\/runtime/, 'react-dom'],
-        plugins: [
-            // these are babel comfigurations
-            babel({
-                exclude: 'node_modules/**',
-                plugins: ['@babel/transform-runtime'],
-                babelHelpers: 'runtime'
-            }),
-            postcss({
-              
-            })
-            // this adds sourcemaps
-            //sourcemaps(),
-            // this adds support for styles
-            // styles({
-            //     postcss: {
-            //         plugins: [
-            //             autoprefixer()
-            //         ]
-            //     }
-            // })
-        ]
-    }
-    config.push(conf)
+  var conf = {
+    input: input,
+    output: {
+      // then name of your package
+      name: "FlapNav",
+      file: `dist/index.${m.fomart}.js`,
+      format: m.fomart,
+      exports: "auto",
+      globals: {
+        react: 'React'
+      }
+    },
+    // this externelizes react to prevent rollup from compiling it
+    external: ["react", /@babel\/runtime/, 'react-dom'],
+    plugins: [
+      // these are babel comfigurations
+      babel({
+        exclude: 'node_modules/**',
+        presets: ["@babel/preset-react"],
+        babelHelpers: "bundled",
+      }),
+
+      postcss({
+
+      }),
+      commonjs()
+    ]
+  }
+  config.push(conf)
 })
 
 export default [
